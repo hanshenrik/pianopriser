89e5a5338dfe103726ace360ae9982c63ab4ef4a
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2024-12-13 14:57:42 +0100

    fix: center theme button

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <header className="w-full">
-                <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <nav className="w-full flex justify-center items-center border-b border-b-foreground/10 h-16">
                   <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                     {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                   </div>
